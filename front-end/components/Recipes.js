fd5d8aa5c7bf2d72e330f93d9b668a6c0ed44774
Senshiben-efrei <ayman.ben.hajjaj@efrei.net>
2022-07-06 12:25:04 +0200

    folder refactor

--- front-end/components/Recipes.js
+++ front-end/components/Recipes.js
@@ -1,9 +1,45 @@
 import React, {useState, useEffect} from 'react';
-import { ScrollView, Center, View, Input, Icon, Pressable, useColorMode, Text } from 'native-base';
+import { ScrollView, Center, View, Input, Icon, Pressable, useColorMode, Text, Flex } from 'native-base';
 import { Ionicons } from "@expo/vector-icons";
 import Recipe from './Recipe';
+import SmallRecipe from './SmallRecipe';
 const Env = require('../Env/EvnVariables')
 const recipeList = require('../src/data/all_recipes.json')
+
+
+
+function Recomendations(props) {
+    const likes = props.likes;
+    if (likes > 4) {
+      return (
+        <View my="2">
+            <Text fontSize="lg" fontWeight="bold" pl="3" pb="2">Recommended for you</Text>
+            <ScrollView flex="1" horizontal={true} showsHorizontalScrollIndicator={true} persistentScrollbar={true}>
+                    <Pressable onPress={() => {
+                        navigation.navigate('recipe', recipeList[5])}}
+                        key={recipeList[5].recipe_id}>
+                        <SmallRecipe item={recipeList[5]} />
+                    </Pressable>
+                    <Pressable onPress={() => {
+                        navigation.navigate('recipe', recipeList[4])}}
+                        key={recipeList[4].recipe_id}>
+                        <SmallRecipe item={recipeList[4]} />
+                    </Pressable>
+                    <Pressable onPress={() => {
+                        navigation.navigate('recipe', recipeList[6])}}
+                        key={recipeList[6].recipe_id}>
+                        <SmallRecipe item={recipeList[6]} />
+                    </Pressable>
+            </ScrollView>
+        </View>
+      );
+    }
+    return (
+        <View backgroundColor="#59DBB7">
+            <Text alignSelf="center" fontSize="2xl" fontWeight="extrabold" color="white" textAlign="center" w="60%" my="5">Like at least 5 recipes to get custom recommendations</Text>
+        </View>
+    );
+  }
 
 export default function Recipes({navigation}) {
     const {
@@ -13,6 +49,7 @@ export default function Recipes({navigation}) {
 
     const [recipe, setRecipe] = useState([]);
     const [search, setSearch] = useState('');
+    const [likes, setLikes] = useState(0);
 
     const GetRecipes = (n = 15) => {
         fetch(`${Env.default.ip}/recipes/${search}`)
@@ -26,6 +63,7 @@ export default function Recipes({navigation}) {
     useEffect(() => {
         // GetRecipes()
         setRecipe(recipeList.splice(0,10))
+        setLikes(0)
     },[])
     return (
         <View backgroundColor={colorMode === "dark" ? "black" : "coolGray.100"}>
@@ -37,7 +75,11 @@ export default function Recipes({navigation}) {
                 minW: "72"
                 }}>
                     <Text>{search}</Text>
-                {recipe.map(recipe => {return (
+                    <View alignSelf="center" w="85%" borderColor="#59DBB7" borderRadius="15" borderWidth="5">
+                        <Recomendations likes={likes} />
+                    </View>
+
+                    {recipe.map(recipe => {return (
                     <Pressable onPress={() => {
                         navigation.navigate('recipe', recipe)}}
                         key={recipe.recipe_id}>
