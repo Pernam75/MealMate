88171d5f2ba01e1a9f3742870520c391a9cbee78
Senshiben-efrei <ayman.ben.hajjaj@efrei.net>
2022-07-14 22:08:24 +0200

    better boarding images

--- front-end/components/Boarding3.js
+++ front-end/components/Boarding3.js
@@ -7,7 +7,7 @@ export default function Borading1({navigation}) {
         <View backgroundColor="white" w="100%" h="100%" alignItems="center">
             <Button borderRadius="10" background="#59DBB7" ml="4" alignSelf="flex-start" mt="10%" onPress={() => navigation.navigate('Boarding2')}><Icon as={Entypo} size="lg" name="chevron-left" color="white" /></Button>
             <AspectRatio w="90%" ratio={1/1} mb="10%" mt="30%">
-            <Image size="100%" borderRadius="30" source={require('../src/images/map.png')} alt="image" />
+            <Image size="100%" borderRadius="30" source={require('../src/images/undraw_my_current_location_re_whmt.png')} alt="image" />
             </AspectRatio>
             <Text fontSize="3xl" fontWeight="bold" mb="40%" color="#59DBB7">Find a Store</Text>
             <HStack space="65%">
